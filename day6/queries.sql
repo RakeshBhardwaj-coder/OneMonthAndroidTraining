8f2ebdfcb7e2ef88acd45e2be7176593f89a1b48
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-07-05 17:37:46 +0530

    Create queries.sql

---
+++ day6/queries.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE My_DB
+
+// in my_db run query
+create table My_Registration(
+  Name Varchar(50),
+    Contact int
+)
+
+// for see
+SELECT * FROM My_Registration
+
+// insert data
+insert into My_Registration(contact, name) values(12346, 'Rakesh' )
+
+// *Note :- download sqlite browser to see data in adroid studio.
